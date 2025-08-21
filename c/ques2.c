58efba9352ed70a8805ff71eb91e5db43b2f1d62
Deepanshishukla2009 <shuklashiri154@gmail.com>
2025-08-21 20:21:49 +0530

    questions of 21-8-25

---
+++ c/ques2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,sum,diff,product,quotient;
+printf("enter value of a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+sum=a+b;
+printf("sum:%d\n",sum);
+diff=a-b;
+printf("diff:%d\n",diff);
+product=a*b;
+printf("product:%d\n",product);
+quotient=a/b;
+printf("quotient:%d\n",quotient);
+return 0;
+}
+
